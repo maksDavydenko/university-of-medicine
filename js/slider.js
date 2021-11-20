4e9f2b46dfd807bac1efe05c6c550d22f1dcd4dd
maksym davydenko <Maksym_Davydenko@epam.com>
2021-11-20 12:04:17 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.header-slider-wrap').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: 500,
+        fade: true,
+    });
+});
