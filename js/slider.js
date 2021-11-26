c6095fd6ecf90ae5c3f1fa66bab083c82178acc2
maksym davydenko <Maksym_Davydenko@epam.com>
2021-11-26 20:44:24 +0200

    add feedback block

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('.header-slider-wrap').slick({
+    $('.header-slider').slick({
         infinite: true,
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        speed: 500,
-        fade: true,
+        prevArrow: "<button type='button' class='slick-prev slick-arrow'><img src='./img/icons/arrow_prev.svg' alt='prev'></button>",
+        nextArrow: "<button type='button' class='slick-next slick-arrow'><img src='./img/icons/arrow_next.svg' alt='next'></i></button>"
     });
 });
