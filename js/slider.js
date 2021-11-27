1b8eac33a78c4efeb5e258c38c33e3cfb10f96ec
maksym davydenko <Maksym_Davydenko@epam.com>
2021-11-27 18:01:44 +0200

    add blocks

--- js/slider.js
+++ js/slider.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
         prevArrow: "<button type='button' class='slick-prev slick-arrow'><img src='./img/icons/arrow_prev.svg' alt='prev'></button>",
         nextArrow: "<button type='button' class='slick-next slick-arrow'><img src='./img/icons/arrow_next.svg' alt='next'></i></button>"
     });
+
+    $('.gallery-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: "<button type='button' class='slick-prev slick-arrow'><img src='./img/icons/arrow_prev.svg' alt='prev'></button>",
+        nextArrow: "<button type='button' class='slick-next slick-arrow'><img src='./img/icons/arrow_next.svg' alt='next'></i></button>"
+    });
 });
+
+
+
