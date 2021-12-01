085ae3d66c1b7b0f66073dee1adebec440f1a558
maksym davydenko <Maksym_Davydenko@epam.com>
2021-12-01 23:24:46 +0200

    adaptive blocks

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('.header-slider').slick({
         infinite: true,
-        adaptiveHeight: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        adaptiveHeight: true,
         prevArrow: "<button type='button' class='slick-prev slick-arrow'><img src='./img/icons/arrow_prev.svg' alt='prev'></button>",
         nextArrow: "<button type='button' class='slick-next slick-arrow'><img src='./img/icons/arrow_next.svg' alt='next'></i></button>"
     });
