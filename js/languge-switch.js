f478254f9818d760bc768c58f55063e2b6fe494d
maksym davydenko <Maksym_Davydenko@epam.com>
2021-12-06 01:42:37 +0200

    add switch language

--- js/languge-switch.js
+++ js/languge-switch.js
@@ -1,37 +1,48 @@
 const LanguageBtn = document.getElementById('change-language');
 const langMenu = document.getElementById('language-menu');
+const langText = document.querySelectorAll('.lang-text');
+
+const closeMenu = () => {
+    langMenu.style.display = 'none';
+    console.log('close');
+}
+
 
 LanguageBtn.addEventListener('click', () => {
     langMenu.style.display = 'block';
-    const list = Array.from(langMenu.children);
+});
 
+langMenu.addEventListener('click', e => {
+    const list = Array.from(langMenu.children);
     list.forEach(item => {
         item.classList.contains('language-active') && item.classList.remove('language-active');
     })
 
     const langitem = e.target.closest('Li');
     langitem.classList.add('language-active');
-
-
-    Array.from(lang.children).forEach(langText => {
-        if (langText.classList.contains('lang-text')) {
-            langText.textContent = langitem.textContent;
-            closeMenu()
-            return;
-        }
-    })
-
-});
-
-
-document.addEventListener('click', e => {
-    const langItem = e.target.closest('#change-language');
-    if (!langItem) {
-        langMenu.style.display = 'none';
-    }
-})
-
-
-const closeMenu = () => {
     langMenu.style.display = 'none';
-}
+
+    const langTextItems = Array.from(langText);
+
+    langTextItems.forEach(item => {
+        item.innerText = langitem.textContent.trim()
+    })
+    console.log(langTextItems);
+    // langText.innerText = langitem.textContent;
+    closeMenu()
+    // Array.from(lang.children).forEach(langText => {
+    //     if (langText.classList.contains('lang-text')) {
+    //         langText.textContent = langitem.textContent;
+    //         closeMenu()
+    //         return;
+    //     }
+    // })
+});
+// document.addEventListener('click', e => {
+//     console.log('this clock');
+//     const langItem = e.target.closest('#change-language');
+//     if (!langItem) {
+//         langMenu.style.display = 'none';
+//     }
+// })
+
---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu');
+const menuList = document.querySelector('.header-nav');
+
+menu.addEventListener('click', () => {
+    menuList.style.display = "none"
+})
