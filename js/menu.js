e712dc4ecd3a96650e0c72b70de92bc0bbd22327
maksym davydenko <Maksym_Davydenko@epam.com>
2021-12-08 18:21:37 +0200

    fix position header

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,23 @@
 const menuBtn = document.getElementById('menu');
 const menuList = document.querySelector('.header-nav-mobile');
 const iconMenu = document.querySelector('.menu-btn');
-const iconClose = document.querySelector('.menu-close')
+const iconClose = document.querySelector('.menu-close');
+const headerWrap = document.querySelector('.header');
 
 menuBtn.addEventListener('click', () => {
 
     if (iconMenu.style.display === 'block') {
         iconClose.style.display = 'block';
         iconMenu.style.display = 'none';
-        menuList.style.display = 'block'
+        menuList.style.display = 'block';
+        headerWrap.style.position = 'relative';
     }
 
     else {
         iconClose.style.display = 'none';
         iconMenu.style.display = 'block';
-        menuList.style.display = 'none'
+        menuList.style.display = 'none';
+        headerWrap.style.position = 'sticky';
     }
 
 })
